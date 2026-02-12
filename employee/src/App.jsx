d005cfb21167952588e5812f88f6c18b62a0eb37
shravyavenishetty <shravyavenishetty03@gmail.com>
2026-02-12 11:51:01 +0530

    Update Employee App configuration for deep UI alignment

--- employee/src/App.jsx
+++ employee/src/App.jsx
@@ -4,6 +4,10 @@ import { AuthProvider, useAuth } from './context/AuthContext';
 import Login from './pages/Login';
 import DashboardLayout from './components/Layout/DashboardLayout';
 import EmployeeDashboard from './pages/Employee/EmployeeDashboard';
+import ApplyLeave from './pages/Employee/ApplyLeave';
+import LeaveHistory from './pages/Employee/LeaveHistory';
+import HolidayCalendar from './pages/Employee/HolidayCalendar';
+import Notifications from './pages/Employee/Notifications';
 import ChangePassword from './pages/ChangePassword';
 
 const PrivateRoute = ({ children }) => {
@@ -28,6 +32,10 @@ function App() {
             </PrivateRoute>
           }>
             <Route index element={<EmployeeDashboard />} />
+            <Route path="apply-leave" element={<ApplyLeave />} />
+            <Route path="leave-history" element={<LeaveHistory />} />
+            <Route path="holidays" element={<HolidayCalendar />} />
+            <Route path="notifications" element={<Notifications />} />
             <Route path="change-password" element={<ChangePassword />} />
           </Route>
         </Routes>
